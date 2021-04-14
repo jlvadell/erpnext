82a3cf1b6d6c9f42bb880de7b444a9c09ec6299a
José Luis Vadell <me@jlvadell.com>
2021-04-14 11:18:17 +0200

    Update setup.py
    refer to this post https://discuss.erpnext.com/t/solved-command-python-setup-py-egg-info-failed-with-error-code-1/44487

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+#from pip.req import parse_requirements
 import re, ast
+
+# applied fix from: https://discuss.erpnext.com/t/solved-command-python-setup-py-egg-info-failed-with-error-code-1/44487
 
 # get version from __version__ variable in erpnext/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open(‘requirements.txt’) as f:
+install_requires = f.read().strip().split(’\n’)
 
 with open('erpnext/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='erpnext',
@@ -21,6 +26,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires,
+	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
